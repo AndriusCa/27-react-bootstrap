d8969cb706545c1d4f3d48e55a452fa2553ebdff
Andrius <andriuscanis@gmail.com>
2023-08-09 15:11:16 +0300

    header, home, hero, footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BasicLayout } from "./layout/BasicLayout";
+import { Home } from "./pages/Home";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+import { Page404 } from "./pages/Page404";
+
 
 
 function App() {
-  return <div className="App">CONTENT</div>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={BasicLayout}>
+            <Route index path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Page404 />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router-dom";
+
+export function Header() {
+  return (
+    <div className="container">
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <div className="col-md-3 mb-2 mb-md-0">
+          <Link
+            to="/"
+            className="d-inline-flex link-body-emphasis text-decoration-none"
+          ></Link>
+        </div>
+
+        <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li>
+            <Link to="/" className="nav-link px-2 link-secondary">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/features" className="nav-link px-2">
+              Features
+            </Link>
+          </li>
+          <li>
+            <Link to="/pricing" className="nav-link px-2">
+              Pricing
+            </Link>
+          </li>
+          <li>
+            <Link to="/faqs" className="nav-link px-2">
+              FAQs
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="nav-link px-2">
+              About
+            </Link>
+          </li>
+        </ul>
+
+        <div className="col-md-3 text-end">
+          <Link to="/login" className="btn btn-outline-primary me-2">
+            Login
+          </Link>
+          <Link to="/register" className="btn btn-primary">
+            Sign-up
+          </Link>
+        </div>
+      </header>
+    </div>
+  );
+}
